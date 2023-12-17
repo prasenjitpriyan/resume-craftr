418fc4260038633c86dcfd61e92665677812e8d0
PRASENJIT DAS <constprasenjit@PRASENJITs-MacBook-Air.local>
2023-12-17 09:46:33 +0530

    New UI added

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../components/Heading'
+
+const Home = () => {
+  return (
+    <div className='bg-[#cbc9ee] h-[100vh] p-0 m-0'>
+      <Heading />
+    </div>
+  )
+}
+
+export default Home
